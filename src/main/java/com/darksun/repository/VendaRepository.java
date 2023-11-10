5c669d803059bc8a7ca1eed2b79e0789bbe10a01
Abel <abelabelao@hotmail.com.br>
2023-11-09 22:32:19 -0300

    Feat: Versão inicial dos repositories

---
+++ src/main/java/com/darksun/repository/VendaRepository.java
@@ -0,0 +1,9 @@
+package com.darksun.repository;
+
+import com.darksun.model.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+}
