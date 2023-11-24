2185129d9aea598b9ce11528948edb2ff8149d4f
Abel <abelabelao@hotmail.com.br>
2023-11-23 23:14:02 -0300

    Feat: Cálculo de lucro

--- src/main/java/com/darksun/controller/MercadoController.java
+++ src/main/java/com/darksun/controller/MercadoController.java
@@ -3,9 +3,12 @@ package com.darksun.controller;
 import com.darksun.model.Produto;
 import com.darksun.service.MercadoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping
@@ -17,5 +20,10 @@ public class MercadoController {
     @PutMapping("/abastecerEstoque/{codigoDeBarras}/{quantidade}")
     public ResponseEntity<Produto> abastecerEstoque (@PathVariable Long codigoDeBarras, @PathVariable Integer quantidade){
         return new ResponseEntity<>(service.abastecerEstoque(codigoDeBarras, quantidade), HttpStatus.OK);
+    }
+
+    @GetMapping("/calcularLucro/{dataInicio}/{dataFim}")
+    public ResponseEntity<Double> calcularLucro (@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicio, @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFim){
+        return new ResponseEntity<>(service.calcularLucro(dataInicio, dataFim), HttpStatus.OK);
     }
 }
--- src/main/java/com/darksun/service/MercadoService.java
+++ src/main/java/com/darksun/service/MercadoService.java
@@ -1,17 +1,34 @@
 package com.darksun.service;
 
 import com.darksun.model.Produto;
+import com.darksun.model.Venda;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class MercadoService {
     @Autowired
     ProdutoService produtoService;
+    @Autowired
+    VendaService vendaService;
 
     public Produto abastecerEstoque (Long codigoDeBarras, Integer quantidade){
         Produto produto = produtoService.buscarPorCodigoDeBarras(codigoDeBarras);
         produtoService.incrementarQuantidadeNoEstoque(produto, quantidade);
         return produto;
+    }
+
+    public Double calcularLucro(LocalDate dataInicio, LocalDate dataFim){
+        double lucro = 0.;
+        for(Venda venda : vendaService.buscar()){
+            if(venda.getDataHoraInicio().isAfter(dataInicio.atStartOfDay()) && venda.getDataHoraFim()!=null && venda.getDataHoraFim().isBefore(dataFim.plusDays(1).atStartOfDay())){
+                for (Produto produto : venda.getProdutos()){
+                    lucro += produto.getPrecoVenda() - produto.getPrecoCompra();
+                }
+            }
+        }
+        return lucro;
     }
 }
